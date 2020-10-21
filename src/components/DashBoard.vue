bb7557fd86f8bfa425e7d9d2ad0fe34f78c8600c
Joris Wijnen <joris.wijnen97@gmail.com>
2020-10-21 10:52:57 +0200

    test new component

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dashboard">
+    <p>I'm a new vue component.</p>
+    <ejs-grid :dataSource="data"> </ejs-grid>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DashBoard",
+    data () {
+    return {
+      data: [
+        { OrderID: 10248, CustomerID: 'VINET', EmployeeID: 5 },
+        { OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6 },
+        { OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4 }]
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
