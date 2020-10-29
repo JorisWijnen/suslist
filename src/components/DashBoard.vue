11d10131c3f240b06dc615af905eac78df9aea0c
Joris Wijnen <joris.wijnen97@gmail.com>
2020-10-29 11:20:22 +0100

    add trashcan to drop unused characters into

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -4,9 +4,18 @@
       <b-col>
         <b-button id="reset" variant="dark" v-on:click="reset">Reset Board</b-button>
       </b-col>
-      <b-col cols='6'>
+      <b-col cols="2">
+        <!-- Placeholder to center title -->
+      </b-col>
+      <b-col cols='4'>
         <h1> Among Us Suspicion List </h1>
-        <p>Drag and drop characters to the separate columns. Click on a character to toggle their alive status.</p>
+        <p>- Drag and drop characters to the separate columns.</p>
+        <p>- Click on a character to toggle their alive status. </p>
+        <p>- Drag a character to the trashcan to remove them from the board.</p>
+      </b-col>
+      <b-col cols="2" title="trashcan">
+        <draggable class="list-group draggable" group="characters">
+        </draggable>
       </b-col>
       <b-col>
         <b-button id="github" variant="dark" target="_blank" href="https://github.com/JorisWijnen/suslist">GitHub Project</b-button>
@@ -14,8 +23,8 @@
     </b-row>
     <b-row id="body">
       <b-col title="Crewmate" style="background-color: #bfff7f;">
-        <h3 class="column-title">Crewmate</h3>
-        <draggable class="list-group draggable" :list="CrewmateList" group="people" @change="log">
+        <draggable class="list-group draggable" :list="CrewmateList" group="characters" draggable=".draggable-item">
+          <h3 slot="header" class="column-title">Crewmate</h3>
           <div class="draggable-item" v-for="(item, index) in CrewmateList" :key="item.color">
             <Card :index="index" :image='item.image' :dead_image='item.dead_image' :alive="item.alive" v-on:toggle="toggle1"/>
           </div>
@@ -23,8 +32,8 @@
       </b-col>
 
       <b-col title="Not suspicous" style="background-color: #feff7f">
-        <h3 class="column-title">Not suspicious</h3>
-        <draggable class="list-group draggable" :list="NotSuspiciousList" group="people" @change="log">
+        <draggable class="list-group draggable" :list="NotSuspiciousList" group="characters" draggable=".draggable-item">
+          <h3 slot="header" class="column-title">Not suspicious</h3>
           <div class="draggable-item" v-for="(item, index) in NotSuspiciousList" :key="item.color">
             <Card :index="index" :image='item.image' :dead_image='item.dead_image' :alive="item.alive" v-on:toggle="toggle2"/>
           </div>
@@ -32,8 +41,8 @@
       </b-col>
 
       <b-col title="Neutral" style="background-color: #fadf7f;">
-        <h3 class="column-title">Neutral</h3>
-        <draggable class="list-group draggable" :list="NeutralList" group="people" @change="log">
+        <draggable class="list-group draggable" :list="NeutralList" group="characters" draggable=".draggable-item">
+          <h3 slot="header" class="column-title">Neutral</h3>
           <div class="draggable-item" v-for="(item, index) in NeutralList" :key="item.color">
             <Card :index="index" :image='item.image' :dead_image='item.dead_image' :alive="item.alive" v-on:toggle="toggle3"/>
           </div>
@@ -41,8 +50,8 @@
       </b-col>
 
       <b-col title="Suspicious" style="background-color: #f7bf7f;">
-        <h3 class="column-title">Suspicious</h3>
-        <draggable class="list-group draggable" :list="SuspiciousList" group="people" @change="log">
+        <draggable class="list-group draggable" :list="SuspiciousList" group="characters" draggable=".draggable-item">
+          <h3 slot="header" class="column-title">Suspicious</h3>
           <div class="draggable-item" v-for="(item, index) in SuspiciousList" :key="item.color">
             <Card :index="index" :image='item.image' :dead_image='item.dead_image' :alive="item.alive" v-on:toggle="toggle4"/>
           </div>
@@ -50,8 +59,8 @@
       </b-col>
 
       <b-col title="Imposter" style="background-color: #f37e7f;">
-        <h3 class="column-title">Imposter</h3>
-        <draggable class="list-group draggable" :list="ImposterList" group="people" @change="log">
+        <draggable class="list-group draggable" :list="ImposterList" group="characters" draggable=".draggable-item">
+          <h3 slot="header" class="column-title">Imposter</h3>
           <div class="draggable-item" v-for="(item, index) in ImposterList" :key="item.color">
             <Card :index="index" :image='item.image' :dead_image='item.dead_image' :alive="item.alive" v-on:toggle="toggle5"/>
           </div>
@@ -91,7 +100,8 @@ export default {
           { color: "Yellow", image: "yellowcrewpng.png", dead_image: "yellowcrewpng-dead.png", alive:true }
         ],
         SuspiciousList: [],
-        ImposterList: []
+        ImposterList: [],
+        TrashcanList: []
       }
     },
     methods: {
@@ -116,7 +126,7 @@ export default {
         this.ImposterList[index].alive = !this.ImposterList[index].alive
       },
       reset() {
-        this.CrewmateList = []; this.NotSuspiciousList = []; this.SuspiciousList = []; this.ImposterList = [];
+        this.CrewmateList = []; this.NotSuspiciousList = []; this.SuspiciousList = []; this.ImposterList = []; this.TrashcanList = [];
         this.NeutralList = [
           { color: "Black", image: "blackcrewpng.png", dead_image: "blackcrewpng-dead.png", alive:true },
           { color: "Blue", image: "bluecrewpng.png", dead_image: "bluecrewpng-dead.png", alive:true },
@@ -137,6 +147,7 @@ export default {
 </script>
 
 <style lang="css">
+
 .icon {
   width: 40px;
   height: 40px;
@@ -146,12 +157,26 @@ export default {
   height: 100%;
 }
 .dashboard #header {
-  min-height:10%;
+  min-height:20vh;
   text-align: center;
 }
 
-.dashboard #header p {
-  display: inline-block;
+.dashboard #header h1, .dashboard #header p {
+  display: block;
+}
+
+.dashboard #header .draggable {
+  align-items: center;
+  min-height: 125px;
+  background-image: url('../assets/trashcan.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.dashboard #header .draggable-item {
+  height: 75%;
+  width: 75%;
 }
 
 .dashboard #header #reset {
@@ -165,7 +190,7 @@ export default {
 }
 
 .dashboard #body {
-  min-height: 90%;
+  min-height: 80vh;
 }
 .dashboard #body .column-title {
   min-height: 5%;
